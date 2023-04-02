809ba1589ff8872d0cd56e79bb8970d24a22d494
Sajay <sajayprakash7@protonmail.com>
2023-04-02 12:33:06 +0000

    remove broken ssh key

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -92,7 +92,6 @@
       isNormalUser = true;
       openssh.authorizedKeys.keys = [
         # TODO: Add your SSH public key(s) here, if you plan on using SSH to connect
-	ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBmJhsUepCSHGcpVWR3ZkADYOUFFWrUpHeAnxZkS9v9+ sajayprakash7@protonmail.com
       ];
       # TODO: Be sure to add any other groups you need (such as networkmanager, audio, docker, etc)
       extraGroups = [ "networkmanager" "wheel" ];
